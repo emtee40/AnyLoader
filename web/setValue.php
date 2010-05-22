809998d69b688ed7a93d957504ef7105782a7478
Jason A. Donenfeld <Jason@zx2c4.com>
2010-05-22 10:50:36 +0200

    Add setValue script.

---
+++ web/setValue.php
@@ -0,0 +1,23 @@
+<?php
+require_once("AnyLoader.php");
+$loader = new AnyLoader();
+
+if ($_GET["value"] == "videoTrack")
+	$loader->setVideoTrack($_GET["movie"], $_GET["track"]);
+elseif ($_GET["value"] == "audioTracks")
+	$loader->setAudioTracks($_GET["movie"], explode(",", $_GET["tracks"]));
+elseif ($_GET["value"] == "terminateRip")
+	$loader->terminateRip();
+elseif ($_GET["value"] == "terminateEncode")
+	$loader->terminateEncode();
+elseif ($_GET["value"] == "terminateUpload")
+	$loader->terminateUpload();
+elseif ($_GET["value"] == "removeMovie")
+	$loader->removeMovie($_GET["movie"]);
+elseif ($_GET["value"] == "startTerminatedTasks")
+	$loader->startTerminatedTasks();
+elseif ($_GET["value"] == "loadISOs")
+	$loader->addRecursiveISOs("/home/zx2c4/AnyLoader");
+
+header("Location: index.php");
+?>
