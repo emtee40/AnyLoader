f543c36a0ae593ac9225ce4e3c28244162f2aaa8
Jason A. Donenfeld <Jason@zx2c4.com>
2010-05-22 23:54:41 +0200

    Remove audio track selection per Avia's specs.

--- web/index.php
+++ web/index.php
@@ -8,12 +8,6 @@ function setVideoTrack(movie)
 	if (!isNaN(track))
 		location.href = "setValue.php?value=videoTrack&movie=" + encodeURIComponent(movie) + "&track=" + track;
 }
-function setAudioTracks(movie)
-{
-	var tracks = prompt("After looking at the list of audio tracks for your desired video track below, choose one or more track by entering the track number(s) here. If you have more than one, seperate them by a comma.\nFor example: 1, 2, 5\n\nAlternatively, type \"auto\" if you would like the audio track to be automatically chosen.", "auto");
-	if (tracks == "auto" || !isNaN(parseInt(tracks.replace(",", "").replace(" ", ""))))
-		location.href = "setValue.php?value=audioTracks&movie=" + encodeURIComponent(movie) + "&tracks=" + tracks.replace(" ", "");
-}
 </script>
 <style>
 a { text-decoration: none; color: blue; }
@@ -55,7 +49,7 @@ echo "<tr><td colspan=2><a href=\"setValue.php?value=startTerminatedTasks\">Star
 echo "</table>";
 echo "<br>";
 echo "<table border=1>";
-echo "<tr><th>Title</th><th>Ripped</th><th>Encoded</th><th>Uploaded</th><th>Video Track</th><th>Audio Tracks</th></tr>";
+echo "<tr><th>Title</th><th>Ripped</th><th>Encoded</th><th>Uploaded</th><th>Video Track</th></tr>";
 foreach ($loader->getTitles() as $title) {
 	echo "<tr><td>".$title->title." (<a href=\"setValue.php?value=removeMovie&movie=".$title->title."\">x</a>)</td><td>".yesno($title->hasRipped)."</td><td>".yesno($title->hasEncoded)."</td><td>".yesno($title->hasUploaded)."</td><td>";
 	if (!$title->hasEncoded && $encodeStatus[0] != $title->title)
@@ -66,23 +60,14 @@ foreach ($loader->getTitles() as $title) {
 		echo $title->videoTrack;
 	if (!$title->hasEncoded && $encodeStatus[0] != $title->title)
 		echo "</a>";
-	echo "</td><td>";
-	if (!$title->hasEncoded && $encodeStatus[0] != $title->title)
-		echo "<a href=\"javascript:setAudioTracks('".$title->title."');\">";
-	if ($title->audioTracks == "auto")
-		echo "Automatically Select";
-	else
-		echo implode(", ", $title->audioTracks);
-	if (!$title->hasEncoded && $encodeStatus[0] != $title->title)
-		echo "</a>";
 	echo "</td></tr>";
 	if (!$title->hasEncoded && $encodeStatus[0] != $title->title) {
-		echo "<tr><td></td><td colspan=5><div style='height:300px;width:700px;overflow:auto'><i>Note: If the movie has already been ripped but not encoded, setting the video track will start the encoding, so if you plan to choose a custom audio track, you should choose the audio track(s) first.</i><pre>";
+		echo "<tr><td></td><td colspan=4><div style='height:300px;width:700px;overflow:auto'><pre>";
 		echo $loader->getTitleInformation($title->title);
 		echo "</pre></div></td></tr>";
 	}
 }
-echo "<tr><th colspan=6><a href=\"setValue.php?value=loadISOs\">Load ISOs From Attached Hard Drive</a></th></tr>";
+echo "<tr><th colspan=5><a href=\"setValue.php?value=loadISOs\">Load ISOs From Attached Hard Drive</a></th></tr>";
 echo "</table>";
 ?>
 </body>
