62d8e6b107f67ac641dab0482c3d12b94c35862a
Jason A. Donenfeld <Jason@zx2c4.com>
2010-05-24 02:02:53 +0200

    Better upload script.

--- installRemote.sh
+++ installRemote.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rsync -vza --exclude=anyloader --exclude=*.o --exclude=moc_*.cpp --exclude Makefile daemon labs.anyclip.com: &&
+rsync -vza --exclude=anyloader --exclude=*.o --exclude=moc_*.cpp --exclude Makefile --delete daemon labs.anyclip.com: &&
 ssh labs.anyclip.com "cd daemon && qmake && make && sudo make install && sudo /etc/init.d/anyloader restart"
 ../GitTools/UploadToWebserver.sh web/
